9096964fd5dc7b179266df78e5d16fc18a42061b
Aaron Dotter <aaron.dotter@gmail.com>
2017-05-02 10:04:58 -0400

    update to GDit.py for saving coefficients

--- GDit.py
+++ GDit.py
@@ -39,13 +39,13 @@ def eq32(omega):
     return sqrt(2./(2.+w2))*pow(1.-w2, 1./12.)*exp(-(4./3.)*w2/pow(2+w2, 3))
 
 
-def stuff(omega,theta): #eq.26, 27, 28; returns rtw, Fw, Teff_ratio
+def stuff(omega,theta): #eq.26, 27, 28; solve the ELR11 equations
     """calculates r~, Teff_ratio, and Flux_ratio"""
 
     #theta is defined as the polar angle.
     #this routine calculates values for 0 <= theta <= pi/2
     #everything else is mapped into this interval by symmetry
-    # theta = 0 at the pole
+    # theta = 0 at the pole(s)
     # theta = pi/2 at the equator
     # -pi/2 < theta < 0: theta -> abs(theta)
     #  pi/2 > theta > pi: theta -> pi - theta
@@ -57,10 +57,12 @@ def stuff(omega,theta): #eq.26, 27, 28; returns rtw, Fw, Teff_ratio
     if omega==0.0: #common sense
         return ones(3)
     
-    else: 
+    else:
+        #first we solve equation 30 for rtw
         q = root(fun=eq30,args=(theta, omega), x0=1.0)
         rtw = asscalar(q['x'])
 
+        #the following are special solutions for extreme values of theta
         w2r3=pow(omega,2)*pow(rtw,3)
 
         if theta==0.0: #pole, eq. 27
@@ -69,12 +71,13 @@ def stuff(omega,theta): #eq.26, 27, 28; returns rtw, Fw, Teff_ratio
         elif theta==0.5*pi: #equator, eq. 28
             Fw = pow(1.0 - w2r3, -f23)
 
-        else: #general case
+        else: #general case for Fw
             q = root(fun=eq24,args=(theta, omega, rtw), x0=theta)
             phi = asscalar(q['x'])
             
             Fw = pow(tan(phi)/tan(theta), 2)
 
+        #equation 31 and similar for Fw
         term1 = pow(rtw,-4)
         term2 = pow(omega,4)*pow(rtw*sin(theta),2)
         term3 = -2*pow(omega*sin(theta),2)/rtw
@@ -84,6 +87,7 @@ def stuff(omega,theta): #eq.26, 27, 28; returns rtw, Fw, Teff_ratio
         
         return rtw, Teff_ratio, Flux_ratio
 
+#convenience functions
 def Rp_div_Re(omega):
     rtw, Teff_ratio, Flux_ratio = stuff(omega, theta=0)
     return rtw
@@ -100,6 +104,7 @@ def Rp_div_R(omega):
 def R_div_Rp(omega):
     return 1.0/Rp_div_R(omega)
 
+#analytical formulas for the surface area and volume of oblate spheroid
 def spheroid_surface_area(Rp,Re):
     #c=polar, a=equatorial; c/a=Rp/Re
     c_div_a=Rp / Re
@@ -114,6 +119,8 @@ def spheroid_surface_area(Rp,Re):
 def spheroid_volume(Rp,Re):
     return 4*pi*Rp*Re*Re/3
 
+#from Binggeli et al. (1980) the following calculates the ratio of semi-major and -minor axes
+#of the ellipse resulting from the projection of an oblate spheroid along the line of sight
 def beta(theta,q): #q is ratio Rp/Re; Binggeli et al. 1980
     j= pow(q*sin(theta),2) + pow(cos(theta),2)
     l=1.0
@@ -121,12 +128,15 @@ def beta(theta,q): #q is ratio Rp/Re; Binggeli et al. 1980
     bottom=j+l+sqrt( pow(j-l,2))
     return sqrt(top/bottom)
 
+#the ares of the projected ellipse; inclication angle is defined differently in this case
 def ellipse(Rp, Re, i): 
     b=beta(theta=pi/2-i,q=Rp/Re)
     return pi*Re*Re*b
 
-def try_again(omega, i, n_nu=50, n_phi=50):
 
+
+def geometric_factors(omega, i, n_nu=50, n_phi=50, do_checks=False):
+    """solves for geometric factors C_T and C_L for arbitrary omega, and inclination"""
     if omega==0: #perfect sphere
         return ones(2)
     
@@ -155,7 +165,6 @@ def try_again(omega, i, n_nu=50, n_phi=50):
     nu_int2=empty(n_nu)
     nu_int3=empty(n_nu)
     nu_int4=empty(n_nu)
-    nu_int5=empty(n_nu)
 
     #loop over polar angle nu
     for j,nu in enumerate(nu_array):
@@ -182,42 +191,51 @@ def try_again(omega, i, n_nu=50, n_phi=50):
             e_mu = array([e_mu_x, e_mu_y, e_mu_z]) / K
 
             dArea = h_nu * h_phi
-            proj = max(0,dot(e_mu,LOS))
+            proj = max(0,dot(e_mu,LOS)) #only projected toward observer
             phi_int1[k] = dArea
             phi_int2[k] = dArea*proj
 
         #phi integral at constant nu
         nu_int1[j] = simps(y=phi_int1,dx=dphi) #total surface area
         nu_int2[j] = simps(y=phi_int2,dx=dphi) #projected area
-        nu_int3[j] = T*nu_int2[j] #integral of Teff_ratio, projected
-        nu_int4[j] = F*nu_int2[j] #integral of Flux_ratio, projected
-        nu_int5[j] = F*nu_int1[j] #integral of Flux_ratio, total
+        nu_int3[j] = F*nu_int2[j] #integral of Flux_ratio, projected
+        nu_int4[j] = F*nu_int1[j] #integral of Flux_ratio, total
 
     #nu integrals
     surface_area = simps(y=nu_int1, dx=dnu)
     projected_area = simps(y=nu_int2, dx=dnu)
-    total_flux = simps(y=nu_int5, dx=dnu)
+    total_flux = simps(y=nu_int4, dx=dnu)
     
-    C_L = 4 * simps(y=nu_int4, dx=dnu) / total_flux
+    C_L = 4 * simps(y=nu_int3, dx=dnu) / total_flux
     C_T = pow( C_L * surface_area / projected_area / 4, 0.25 )
-    
-    surface_area_check = abs( surface_area / spheroid_surface_area(Rp,Re) - 1.0)
-    projected_area_check = abs( projected_area / ellipse(Rp, Re, i) - 1.0)
 
-    if surface_area_check > 0.001 or projected_area_check > 0.001:
-        print(' omega, i = {0:n}, {1:n}'.format(omega,i))
-        print(' surface area ratio: {0:n}'.format(surface_area_ratio))
-        print(' projected area ratio: {0:n}'.format(projected_area_ratio))
+    if do_checks:
+        surface_area_check = abs( surface_area / spheroid_surface_area(Rp,Re) - 1.0)
+        projected_area_check = abs( projected_area / ellipse(Rp, Re, i) - 1.0)
+
+        if surface_area_check > 0.001 or projected_area_check > 0.001:
+            print(' omega, i = {0:n}, {1:n}'.format(omega,i))
+            print(' surface area ratio: {0:n}'.format(surface_area_ratio))
+            print(' projected area ratio: {0:n}'.format(projected_area_ratio))
         
     return C_T, C_L
 
-def save_coefficients(n):
+
+
+#compute the coefficients C_T and C_L on an nxn matrix
+def save_coefficients(n,output='GD.npz'):
     omega=linspace(0,1,n)
     inclination=linspace(0,pi/2,n)
     C_T=empty((n,n))
     C_L=empty((n,n))
+    #not finished!
+    #use savez
+    for i,omega in enumerate(omega):
+        for j,incl in enumerate(inclination):
+            C_T[j,i], C_L[j,i] = geometric_factors(omega, incl)
+    savez(output, C_T=C_T, C_L=C_L)
 
-
+#returns instance of the BivariateRect
 def create_interpolants(npz):
     data=load(npz)
     C_L=data['C_L']
